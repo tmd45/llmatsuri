2c6f51a68674a0f2f5974d5a25a749dfd53be4dd
Yoko TAMADA <tamada@feedforce.jp>
2013-08-22 14:21:55 +0900

    とりあえず出来た。またテストとか書いてないー

---
+++ ll_quiz_2013.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+File.foreach("input.txt").with_index do |line, index|
+  #puts "#{index}: #{line}"
+  splited = line.split
+  draw = splited[1].gsub(splited[0], '#').gsub(/\d/, ' ')
+  puts "#{index}: #{draw}"
+end
+
