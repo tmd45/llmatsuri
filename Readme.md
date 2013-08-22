604fa3279d2d09c0506c71e7e025bbbdcb133cb4
Yoko TAMADA <tamada@feedforce.jp>
2013-08-22 14:25:01 +0900

    一応、実行方法を追記

--- Readme.md
+++ Readme.md
@@ -4,6 +4,11 @@ LL Matsuri
 LL QUIZ 2013
 ------------
 
-* http://ll.jus.or.jp/2013/quiz.html
+http://ll.jus.or.jp/2013/quiz.html
 
 今年は Ruby で！
+
+* 実行方法
+** input.txt に問題を書き込んで以下で実行。
+
+ ruby ./ll_quiz_2013.rb
--- ll_quiz_2013.rb
+++ ll_quiz_2013.rb
@@ -4,6 +4,7 @@ File.foreach("input.txt").with_index do |line, index|
   #puts "#{index}: #{line}"
   splited = line.split
   draw = splited[1].gsub(splited[0], '#').gsub(/\d/, ' ')
+
   puts "#{index}: #{draw}"
 end
 
